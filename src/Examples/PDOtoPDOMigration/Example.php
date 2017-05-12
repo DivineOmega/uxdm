0bd8b3669246b2b3eaa04c4485b5866edc303efc
Jordan Hall <jordan.hall@rapidweb.biz>
2017-05-12 14:15:39 +0100

    Simplify PDO example

--- src/Examples/PDOtoPDOMigration/Example.php
+++ src/Examples/PDOtoPDOMigration/Example.php
@@ -6,11 +6,9 @@ use RapidWeb\uxdm\Objects\Sources\PDOSource;
 use RapidWeb\uxdm\Objects\Destinations\PDODestination;
 use RapidWeb\uxdm\Objects\Migrator;
 
-$pdo = new PDO('mysql:dbname=laravel-test;host=127.0.0.1', 'root', getenv('UXDM_EXAMPLE_PASSWORD'));
-$pdoSource = new PDOSource($pdo, 'users');
+$pdoSource = new PDOSource(new PDO('mysql:dbname=laravel-test;host=127.0.0.1', 'root', getenv('UXDM_EXAMPLE_PASSWORD')), 'users');
 
-$pdo2 = new PDO('mysql:dbname=new-test;host=127.0.0.1', 'root', getenv('UXDM_EXAMPLE_PASSWORD'));
-$pdoDestination = new PDODestination($pdo2, 'new_users');
+$pdoDestination = new PDODestination(new PDO('mysql:dbname=new-test;host=127.0.0.1', 'root', getenv('UXDM_EXAMPLE_PASSWORD')), 'new_users');
 
 $migrator = new Migrator;
 $migrator->setSource($pdoSource)
