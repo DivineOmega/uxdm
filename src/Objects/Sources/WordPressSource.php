5bef50b891f86fedc8141e6e6bd8881140d4fb44
DivineOmega <jordan@hall05.co.uk>
2017-05-28 18:23:56 +0100

    Initial work on WordPress source object

---
+++ src/Objects/Sources/WordPressSource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace RapidWeb\uxdm\Objects\Sources;
+
+use RapidWeb\uxdm\Interfaces\SourceInterface;
+use RapidWeb\uxdm\Objects\DataRow;
+use RapidWeb\uxdm\Objects\DataItem;
+use PDO;
+use PDOStatement;
+use Exception;
+
+class WordPressSource implements SourceInterface
+{
+    private $pdo;
+    private $fields = [];
+    private $postType;
+
+    public function __construct(PDO $pdo, $postType = 'post') {
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo = $pdo;
+        $this->postType = $postType;
+        $this->fields = $this->getPostFields();
+    }
+
+    private function getPostFields() {
+        $sql = 'select * from wp_posts where post_type = '.$this->postType.' limit ? , ?';
+        
+        $stmt = $this->pdo->prepare($sql);
+        $this->bindLimitParameters($stmt, 0, 1);
+
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $postFields = array_keys($row);
+        return $postFields;
+    }
+
+    private function getSQL($fieldsToRetrieve) {
+
+        $fieldsSQL = implode(', ', $fieldsToRetrieve);
+
+        $sql = 'select '.$fieldsSQL.' from '.$this->tableName;
+
+        foreach($this->joins as $join) {
+            $sql .= $join->getSQL();
+        }
+
+        $sql .= ' limit ? , ?';
+
+        if ($this->overrideSQL) {
+            $sql = $this->overrideSQL;
+        }
+        return $sql;
+    }
+
+    private function bindLimitParameters(PDOStatement $stmt, $offset, $perPage) {
+        $stmt->bindValue(1, $offset, PDO::PARAM_INT);
+        $stmt->bindValue(2, $perPage, PDO::PARAM_INT);
+    }
+
+    public function getDataRows($page = 1, $fieldsToRetrieve = []) {
+
+        $perPage = 10;
+
+        $offset = (($page-1) * $perPage);
+
+        $sql = $this->getSQL($fieldsToRetrieve);
+        
+        $stmt = $this->pdo->prepare($sql);
+        $this->bindLimitParameters($stmt, $offset, $perPage);
+
+        $stmt->execute();
+
+        $dataRows = [];
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $dataRow = new DataRow;
+            
+            foreach($row as $key => $value) {
+                $dataRow->addDataItem(new DataItem($key, $value));
+            }
+
+            $dataRows[] = $dataRow;
+        }
+
+        return $dataRows;
+
+    }
+
+    public function getFields() {
+        return $this->fields;
+    }
+}
