5730719dbe26ad2e21b09359990ee84b8b7d09e5
Jordan Hall <jordan.hall@rapidweb.biz>
2017-06-06 13:52:41 +0100

    XML Source and example file

---
+++ src/Examples/XMLtoCSVMigration/Example.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__.'/../../../vendor/autoload.php';
+
+use RapidWeb\uxdm\Objects\Sources\XMLSource;
+use RapidWeb\uxdm\Objects\Destinations\CSVDestination;
+use RapidWeb\uxdm\Objects\Migrator;
+
+$xmlSource = new XMLSource(__DIR__.'/source.xml', '/ns:urlset/ns:url');
+$xmlSource->addXMLNamespace('ns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+var_dump($xmlSource->getFields());
+
+$csvDestination = new CSVDestination(__DIR__.'/destination.csv');
+
+$migrator = new Migrator;
+$migrator->setSource($xmlSource)
+         ->setDestination($csvDestination)
+         ->migrate();
---
+++ src/Objects/Sources/XMLSource.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace RapidWeb\uxdm\Objects\Sources;
+
+use RapidWeb\uxdm\Interfaces\SourceInterface;
+use RapidWeb\uxdm\Objects\DataRow;
+use RapidWeb\uxdm\Objects\DataItem;
+use DOMDocument;
+use DOMXPath;
+use Exception;
+
+class XMLSource implements SourceInterface
+{
+    private $xpath;
+    private $xpathQuery;
+    private $fields = [];
+
+    public function __construct($file, $xpathQuery, $namespaces = []) {
+
+        $doc = new DOMDocument();
+        $doc->load($file);
+
+        $this->xpath = new DOMXPath($doc);
+        $this->xpathQuery = $xpathQuery;
+    }
+
+    public function addXMLNamespace($prefix, $namespaceURI) {
+        $this->xpath->registerNamespace($prefix, $namespaceURI);
+    }
+
+    private function getXMLFields() {
+        
+        $fields = [];
+
+        $domNodeList = $this->xpath->query($this->xpathQuery);
+
+        if(!$domNodeList || !$domNodeList->length) {
+            throw new Exception('Xpath query is invalid or points to a non-existant DOM element.');
+        }
+
+        foreach($domNodeList as $domElement) {
+        
+            foreach($domElement->childNodes as $childNode) {
+                $fields[] = $childNode->nodeName;
+            }
+
+        }
+
+        $fields = array_unique($fields);
+
+        return $fields;
+    }
+
+    public function getDataRows($page = 1, $fieldsToRetrieve = []) {
+
+        $perPage = 10;
+
+        $offset = (($page-1) * $perPage);
+        
+        $domNodeList = $this->xpath->query($this->xpathQuery);
+
+        $dataRows = [];
+
+        $count = 0;
+
+        foreach($domNodeList as $domElement) {
+
+            if ($count >= $offset && $count < $offset + $perPage) {
+
+                $dataRow = new DataRow;
+                
+                foreach($domElement->childNodes as $childNode) {
+
+                    if (in_array($childNode->nodeName, $fieldsToRetrieve)) {
+                        $dataRow->addDataItem(new DataItem($childNode->nodeName, $childNode->nodeValue));
+                    }
+                }
+
+                $dataRows[] = $dataRow;
+
+            }
+
+            if($count >= $offset + $perPage) {
+                break;
+            }
+
+            $count++;
+        }
+
+        return $dataRows;
+
+    }
+
+    public function getFields() {
+
+        if (!$this->fields) {
+            $this->fields = $this->getXMLFields();
+        }
+
+        return $this->fields;
+    }
+}
