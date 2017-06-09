02e9504f921b5427ae8e1efbc72b3d3e8fe71abb
Jordan Hall <jordan.hall@rapidweb.biz>
2017-06-09 09:51:49 +0100

    Added AssociativeArrayDestination

---
+++ src/Objects/Destinations/AssociativeArrayDestination.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RapidWeb\uxdm\Objects\Destinations;
+
+use RapidWeb\uxdm\Interfaces\DestinationInterface;
+
+class AssociativeArrayDestination implements DestinationInterface
+{
+    private $array;
+
+    public function __construct(&$array) {
+        $this->array = $array;
+    }
+
+    public function putDataRows(array $dataRows) {
+        
+        foreach($dataRows as $dataRow) {
+
+            $dataItems = $dataRow->getDataItems();
+
+            $row = [];
+
+            foreach($dataItems as $dataItem) {
+                $row[$dataItem->field] = $dataItem->value;
+            }
+            
+            $this->array[] = $row;
+        }
+    }
+}
