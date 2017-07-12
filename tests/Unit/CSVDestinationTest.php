167815e7fea33467234d295018a27218305ef613
Jordan Hall <jordan.hall@rapidweb.biz>
2017-07-12 12:17:22 +0100

    CSV Destination Test

---
+++ tests/Unit/CSVDestinationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use RapidWeb\uxdm\Objects\DataRow;
+use RapidWeb\uxdm\Objects\DataItem;
+use RapidWeb\uxdm\Objects\Destinations\CSVDestination;
+
+final class CSVDestinationTest extends TestCase
+{
+    private function createDataRows()
+    {
+        $faker = Faker\Factory::create();
+
+        $dataRows = [];
+
+        $dataRow = new DataRow;
+        $dataRow->addDataItem(new DataItem('name', $faker->word));
+        $dataRow->addDataItem(new DataItem('value', $faker->randomNumber));
+        $dataRows[] = $dataRow;
+
+        $dataRow = new DataRow;
+        $dataRow->addDataItem(new DataItem('name', $faker->word));
+        $dataRow->addDataItem(new DataItem('value', $faker->randomNumber));
+        $dataRows[] = $dataRow;
+
+        return $dataRows;
+    }
+
+    public function testPutDataRows()
+    {
+        $dataRows = $this->createDataRows();
+
+        $file = __DIR__.'/Data/destination.csv';
+
+        $destination = new CSVDestination($file);
+        $destination->putDataRows($dataRows);
+
+        $fileContent = file_get_contents($file);
+
+        $expectedFileContent = 'name,value'.PHP_EOL;
+        
+        foreach($dataRows as $dataRow) {
+            $expectedFileContent .= $dataRow->getDataItemByFieldName('name')->value;
+            $expectedFileContent .= ',';
+            $expectedFileContent .= $dataRow->getDataItemByFieldName('value')->value;
+            $expectedFileContent .= PHP_EOL;
+        }
+
+        $this->assertEquals($expectedFileContent, $fileContent);        
+    }
+
+}
