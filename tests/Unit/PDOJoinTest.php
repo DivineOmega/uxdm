ec8465cf3d5ae6194910480c290f8d93648d78d5
Jordan Hall <jordan.hall@rapidweb.biz>
2017-07-12 11:11:54 +0100

    PDO Join test

---
+++ tests/Unit/PDOJoinTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use RapidWeb\uxdm\Objects\Sources\PDO\Join;
+
+final class PDOJoinTest extends TestCase
+{
+
+    public function testGetJoinSQL()
+    {
+        $table = 'table1';
+        $key = 'key2';
+        $foreignKey = 'foreignKey3';
+
+        $join = new Join($table, $key, $foreignKey);
+
+        $expectedSQL = ' INNER JOIN '.$table.' ON '.$key.' = '.$foreignKey;
+
+        $this->assertEquals($expectedSQL, $join->getSQL());
+        
+    }
+
+}
