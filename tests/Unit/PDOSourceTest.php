d64f3222a1695c18ad5a862bbfd5f90b8c9c5d1b
Jordan Hall <jordan.hall@rapidweb.biz>
2017-07-13 10:47:37 +0100

    Override SQL tests

--- tests/Unit/PDOSourceTest.php
+++ tests/Unit/PDOSourceTest.php
@@ -78,4 +78,43 @@ final class PDOSourceTest extends TestCase
         $this->assertCount(0, $dataRows);
     }
 
+    public function testOverrideSQLWithoutSelect()
+    {
+        $source = $this->createPDOSource();
+
+        $this->expectException(Exception::class);
+
+        $source->setOverrideSQL('invalid sql');
+    }
+
+    public function testOverrideSQLWithoutLimit()
+    {
+        $source = $this->createPDOSource();
+
+        $this->expectException(Exception::class);
+
+        $source->setOverrideSQL('select * from users where name = \'Tim\'');
+    }
+
+    public function testOverrideSQL()
+    {
+        $source = $this->createPDOSource();
+
+        $source->setOverrideSQL('select name, email from users where name = \'Tim\' limit ? , ?');
+
+        $dataRows = $source->getDataRows(1);
+
+        $this->assertCount(1, $dataRows);
+
+        $dataItems = $dataRows[0]->getDataItems();
+
+        $this->assertCount(2, $dataItems);
+
+        $this->assertEquals('name', $dataItems[0]->fieldName);
+        $this->assertEquals('Tim', $dataItems[0]->value);
+
+        $this->assertEquals('email', $dataItems[1]->fieldName);
+        $this->assertEquals('tim@example.com', $dataItems[1]->value);
+    }
+
 }
